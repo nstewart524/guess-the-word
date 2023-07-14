a6c927b9070ac6fb178eeb3178749c6ae5fb7d37
Nikolas Stewart <nstewart524@Nikolass-Air.attlocal.net>
2023-07-14 13:57:18 -0500

    Validated player guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(`.guessed-letters`)
+const guessedLettersE = document.querySelector(`.guessed-letters`)
 const guessButton = document.querySelector(`.guess`)
 const textInput = document.querySelector(`.letter`)
 const inProgress = document.querySelector(`.word-in-progress`)
@@ -8,14 +8,14 @@ const message = document.querySelector(`.message`)
 const playButton = document.querySelector(`.play-again`)
 
 const word = `magnolia`
-
+const guessedLetters = []
 
 
 
 const placeholder = function (word) {
   const placeholderLetters = []
   for (const letter of word) {
-    console.log(letter)
+    // console.log(letter)
     placeholderLetters.push(`●`)
   }
   inProgress.innerText = placeholderLetters.join(``)
@@ -26,9 +26,41 @@ placeholder(word)
 
 
 
-guessButton.addEventListener(`click`, function(e) {
+guessButton.addEventListener(`click`, function (e) {
   e.preventDefault()
+  message.innerText = ``
   const inputValue = textInput.value
-  console.log(inputValue)
+  // console.log(inputValue)
+  const validGuess = validate(inputValue)
+
+  if (validGuess) {
+    // We've got a letter! Let's guess!
+    makeGuess(inputValue)
+  }
   textInput.value = ``
 })
+
+
+const validate = function (inputValue) {
+  const acceptedLetter = /[a-zA-Z]/
+  if (inputValue.length === 0) {
+    message.innerText = `Please enter any letter.`
+  } else if (inputValue.length > 1) {
+    message.innerText = `Please enter one letter.`
+  } else if (inputValue.match(acceptedLetter)) {
+    message.innerText = `Please enter a single letter.`
+  } else {
+    return inputValue
+  }
+}
+
+
+const makeGuess = function (inputValue) {
+  inputValue = inputValue.toUpperCase()
+  if (guessedLetters.includes(inputValue)) {
+    message.innerText = `You already made that selection, Try again.`
+  } else {
+    guessedLetters.push(inputValue)
+    console.log(guessedLetters)
+  }
+}
